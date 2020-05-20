75f8aaaa02470785fc44fbce1747d84751a92621
Chris Matian <chris.matian@race.com>
2020-05-19 18:07:22 -0700

    documentation and txt removal

--- gw.py
+++ gw.py
@@ -7,6 +7,14 @@ from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+"""
+Selenium is used instead of traditional, headless scraping techniques due to GW's use of Endeca (e-commerce toolkit from Oracle). They inject their inventory data into the window after it has finished loading. From my experimentation, I could technically parse the document without BS4, but that would take a long time to figure out an algorithm (most likely RegEx) that can accurately pull the correct data points from their manifest.
+
+Selenium is used as a driver for navigating to a specific URL, and quickly pulling the source after they inject it into the browser window. A headless browser window can be used but I would need to overcomplicate the code with overrides to hide the fact that it's a bot scraping their site. Recaptcha will otherwise block access.
+
+A viewable window gets around both of the above issues and is a lot simpler for my needs.
+"""
+
 
 class GamesWorkshopInventory:
     def __init__(self):
